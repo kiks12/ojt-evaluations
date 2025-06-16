c976a752a19dcab711e925aad7f687a5c56ce6ca
Francis James Tolentino <francistolentino1107@gmail.com>
2025-06-16 22:52:45 +0800

    vim magic

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 
 from transformers import pipeline
-data = ["I love you", "I hate you"]
 
-specific_model = pipeline(model="finiteautomata/bertweet-base-sentiment-analysis")
-print(specific_model(data))
+sentiment_model = pipeline(model="finiteautomata/bertweet-base-sentiment-analysis")
+
+from typing import Union
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Statement(BaseModel):
+    text: str
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.post("/statement/")
+def analyze_statement(statement: Statement):
+    return sentiment_model(statement.text)
+
