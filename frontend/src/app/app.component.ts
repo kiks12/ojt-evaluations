7b6798fb9b272771786faaab3a892991c9998acf
Francis James Tolentino <francistolentino1107@gmail.com>
2025-06-17 23:05:44 +0800

    UI Change; httpClient provider added

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { EvaluationFormComponent } from "./components/evaluation-form/evaluation-form.component";
+import { StatementScoreComponent } from "./components/statement-score/statement-score.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, EvaluationFormComponent, StatementScoreComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
