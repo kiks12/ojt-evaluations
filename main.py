78cc0f9b8a8aafca508eaab328756a68b3f40f55
Francis James Tolentino <francistolentino1107@gmail.com>
2025-06-17 23:05:11 +0800

    CORS middleware added

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
 
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI
+from typing import Union
 from transformers import pipeline
 
-sentiment_model = pipeline(model="finiteautomata/bertweet-base-sentiment-analysis")
+sentiment_model = pipeline(
+    model="finiteautomata/bertweet-base-sentiment-analysis")
 
-from typing import Union
-from fastapi import FastAPI
-from pydantic import BaseModel
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+    "http://localhost:4200"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
 
 class Statement(BaseModel):
     text: str
@@ -17,7 +34,7 @@ class Statement(BaseModel):
 def read_root():
     return {"Hello": "World"}
 
+
 @app.post("/statement/")
 def analyze_statement(statement: Statement):
     return sentiment_model(statement.text)
-
