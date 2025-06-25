6bac59a4ecc883b0cbc83e3fb2ca223d3634ea95
Francis James Tolentino <francistolentino1107@gmail.com>
2025-06-25 22:22:28 +0800

    Added form routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { FormsComponent } from './components/forms/forms.component';
 import { EvaluationFormComponent } from './components/evaluation-form/evaluation-form.component';
 import { EditFormComponent } from './components/forms/edit-form/edit-form.component';
+import { FormFillComponent } from './components/form-fill/form-fill.component';
+import { FormPreviewComponent } from './components/form-preview/form-preview.component';
 
 export const routes: Routes = [
   {
@@ -13,7 +15,20 @@ export const routes: Routes = [
     component: EvaluationFormComponent
   },
   {
-    path: "forms/:id",
+    path: "forms/:id/edit",
     component: EditFormComponent
+  },
+  {
+    path: "forms/:id/preview",
+    component: FormPreviewComponent
+  },
+  {
+    path: "forms/:id/fill",
+    component: FormFillComponent
+  },
+  {
+    path: "forms/:id",
+    redirectTo: "forms/:id/edit",
+    pathMatch: "full"
   }
 ];
